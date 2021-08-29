ac48a92d8d93e8a4a09f6b4583305f2b2d84872a
Beda Schmid <hello@tukutoi.com>
2021-08-29 17:21:04 +0700

    08-29-2021 2
    ### 1.0.1
    * Updated Readme

--- tkt-contact-form.php
+++ tkt-contact-form.php
@@ -15,7 +15,7 @@
  * Plugin Name:       TukuToi Contact Form
  * Plugin URI:        https://plugin.com/tkt-contact-form-uri/
  * Description:       A simple Contact Form for ClassicPress.
- * Version:           1.0.0
+ * Version:           1.0.1
  * Author:            TukuToi
  * Requires at least: 1.0.0
  * Tested up to:      5.8
@@ -36,7 +36,7 @@ if ( ! defined( 'WPINC' ) ) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'TKT_CONTACT_FORM_VERSION', '1.0.0' );
+define( 'TKT_CONTACT_FORM_VERSION', '1.0.1' );
 
 /**
  * The code that runs during plugin activation.
